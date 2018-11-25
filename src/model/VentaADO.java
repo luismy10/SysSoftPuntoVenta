3c03cad65644babc7932e95cebf436a67d35250a
Rubert <ruberfc@gmail.com>
2018-11-25 13:32:39 -0500

    Actualizado

--- src/model/VentaADO.java
+++ src/model/VentaADO.java
@@ -8,6 +8,8 @@ package model;
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableView;
 import javax.xml.bind.DatatypeConverter;
 
 /**
@@ -97,4 +99,80 @@ public class VentaADO {
             }
         }
     }
+    
+    private String InsertDetalleVenta(DetalleVentaTB detalleVentaTB, TableView<ArticuloTB> tableView, ObservableList<LoteTB> loteTBs) throws SQLException{
+        
+        PreparedStatement venta = null;
+        
+        CallableStatement codigo_venta = null;
+        PreparedStatement detalle_venta = null;
+        
+        PreparedStatement articulo_update = null;
+        
+        PreparedStatement lote_compra = null;
+        
+        try{
+            DBUtil.dbConnect();
+            DBUtil.getConnection().setAutoCommit(false);
+            codigo_venta = DBUtil.getConnection().prepareCall("{? = call Fc_Serie_Numero_Generado()}");
+            codigo_venta.registerOutParameter(1, java.sql.Types.VARCHAR);
+            codigo_venta.execute();
+            
+            String id_venta = codigo_venta.getString(1);
+            
+            detalle_venta = DBUtil.getConnection().prepareStatement("INSERT INTO DetalleVentaTB(IdVenta,IdArticulo,Cantidad,PrecioUnitario,Descuento,Importe)"
+                    + "VALUES(?,?,?,?,?,?)");
+            articulo_update = DBUtil.getConnection().prepareStatement("UPDATE ArticuloTB SET Cantidad = Cantidad + ? WHERE IdArticulo = ?");
+            
+            for (int i = 0; i < tableView.getItems().size(); i++) {
+                detalle_venta.setString(1, id_venta);
+                detalle_venta.setString(2, tableView.getItems().get(i).getIdArticulo());
+                detalle_venta.setInt(3, detalleVentaTB.getCantidad());
+                detalle_venta.setDouble(4, detalleVentaTB.getPrecioUnitario());
+                detalle_venta.setDouble(5, detalleVentaTB.getDescuento());
+                detalle_venta.setDouble(6, detalleVentaTB.getImporte());
+                detalle_venta.addBatch();
+
+                articulo_update.setDouble(3, tableView.getItems().get(i).getCantidad());
+                //articulo_update.setString(4, tableView.getItems().get(i).getIdArticulo());
+                articulo_update.addBatch();              
+
+            }
+            
+            detalle_venta.executeBatch();
+            articulo_update.executeBatch();
+            DBUtil.getConnection().commit();
+            return "register";
+            
+        }catch(SQLException ex){
+                      
+            try {
+                DBUtil.getConnection().rollback();
+                return ex.getLocalizedMessage();
+            } catch (SQLException ex1) {
+                return ex1.getLocalizedMessage();
+            }
+        }finally {
+            try {
+//                if (venta != null) {
+//                    venta.close();
+//                }
+                if (detalle_venta != null) {
+                    detalle_venta.close();
+                }
+                if (articulo_update != null) {
+                    articulo_update.close();
+                }
+                if (codigo_venta != null) {
+                    codigo_venta.close();
+                }
+//                if (lote_compra != null) {
+//                    lote_compra.close();
+//                }
+                DBUtil.dbDisconnect();
+            } catch (SQLException e) {
+            }
+        }
+    }
+    
 }
